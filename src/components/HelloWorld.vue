8bf4432d5059e3a9cf044334470d44441f58c757
John Ericsson <ercsssn@gmail.com>
2022-12-19 14:37:30 +0800

    Refactor hours

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,19 +1,16 @@
 <template>
-  <p v-if="isMorning">Good morning!</p>
-  <p v-else-if="isAfternoon">Good afternoon!</p>
-  <p v-else-if="isEvening">Good evening!</p>
+  <p v-if="hours < 12">Good morning!</p>
+  <p v-else-if="hours >= 12 && hours < 18">Good afternoon!</p>
+  <p v-else-if="hours >= 18">Good evening!</p>
 </template>
 
 <script>
-var hours = new Date().getHours();
 
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      isMorning: hours < 12,
-      isAfternoon: hours >= 12 && hours < 18,
-      isEvening: hours >= 18
+      hours: new Date().getHours()
 
     }
   }
